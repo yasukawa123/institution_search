b84e6ddc4072d6214591ab1b85b8468f2beb2d2c
yasukawa123 <kazu050107@gmail.com>
2022-06-06 13:06:22 +0900

    Auth ヘッダーにログイン機能の追加

--- src/institution_search/resources/views/index.blade.php
+++ src/institution_search/resources/views/index.blade.php
@@ -20,8 +20,37 @@
                     <li class="nav-item"><a href="#">BLANDについて</a></li>
                     <li class="nav-item"><a href="#">予約について</a></li>
                     <li class="nav-item"><a href="#">お問合せ</a></li>
-                    <li class="nav-item"><a href="#">会員登録</a></li>
-                    <li class="nav-item"><a href="#">ログイン</a></li>
+                    @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('会員登録') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                 </ul>
                 </nav>
             </div>
