edd13dd349a4fc6f1358da825e6530ae302489cb
yasukawa123 <kazu050107@gmail.com>
2022-06-14 01:27:58 +0900

    詳細ページ：controller showの修正

--- src/institution_search/resources/views/shop/show.blade.php
+++ src/institution_search/resources/views/shop/show.blade.php
@@ -5,16 +5,22 @@
     <table class="table table-striped">
   <thead>
     <tr>
+      <th>ショップ画像</th>
       <th>ショップID</th>
       <th>店名</th>
-      <th>作成日</th>
+      <th>ショップ説明</th>
+      <th>住所</th>
+      <th>ショッププラン</th>
     </tr>
   </thead>
   <tbody>
     <tr>
+      <td>{{ $shop->images }}</td>
       <td>{{ $shop->id }}</td>
       <td>{{ $shop->name }}</td>
-      <td>{{ $shop->created_at }}</td>
+      <td>{{ $shop->introduction_text }}</td>
+      <td>{{ $shop->prefecture }} {{ $shop->city }}{{ $shop->unique_name }}</td>
+      <td>{{ $shop->shop_plan_id }}</td>
     </tr>
   </tbody>
 </table>
