95702e2b7a0967390b393f64a4366158b5d93590
yasukawa123 <kazu050107@gmail.com>
2022-06-14 16:19:46 +0900

    デザイン：トップページ：検索欄：レイアウト修正

--- src/institution_search/resources/views/index.blade.php
+++ src/institution_search/resources/views/index.blade.php
@@ -59,27 +59,16 @@
         <main>
             <div class="top-image-container">
                 <!-- <img src="/images/top-image03.jpg" alt="トップイメージ"> -->
-                <h1>どんな未来に泊まろう。</h1>
-
-                <div class="container">
-                    <ul class="search-nav-wrapper">
-                        <li class="search_nav_item">場所： <input type="search" id="name" name="name" required minlength="4" maxlength="8" size="10"></li>
-                        <li class="search_nav_item">金額： <input type="number" id="name" name="name" required minlength="4" maxlength="8" size="10"></li>
-                        <li class="search_nav_item">人数：
-                            <select name="人数">
-                                <option value="tokyo">1</option>
-                                <option value="kanagawa">2</option>
-                                <option value="chiba">3</option>
-                                <option value="saitama">4</option>
-                                <option value="ibaraki">5</option>
-                                <option value="ibaraki">6</option>
-                                <option value="ibaraki">7人以上</option>
-                            </select>
-                        </li>
-                        <li class="search_nav_icon">
-                            <button type="submit" id="sbtn2"><img src="/images/icons/icon-search.png" alt="予約検索"></button>
-                        </li>
-                    </ul>
+                <h1>どんな未体験に泊まろう。</h1>
+                <div class="container">                    
+                    <form id="form1" action="自分のサイトURL">
+                        <div class="button-justify">
+                            <input id="sbox"  id="s" name="s" type="text" placeholder="都道府県を入力" />
+                            <input id="sbox"  id="s" name="s" type="text" placeholder="金額を入力" />
+                            <input id="sbox"  id="s" name="s" type="text" placeholder="人数を入力" />
+                            <input id="sbtn" type="submit" value="検索" /> 
+                        </div>
+                    </form>                                
                 </div>
             </div>
             
